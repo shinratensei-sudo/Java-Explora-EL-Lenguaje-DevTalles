fb919c3509493d1742a347cde27b21ee661c0549
Fernando Juarez <fernandojs459@gmail.com>
2025-06-15 12:41:43 -0600

    variables primitivas

---
+++ 02-variables-primitivas/src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        long age = 23;
+        System.out.println("age = " + age);
+        float price = 19.99F;
+        int xAxis, yAxis;
+        xAxis=10;
+        yAxis=20;
+        age=30;
+        System.out.println("age = " + age);
+
+        System.out.println("xAxis = " + xAxis);
+        System.out.println("yAxis = " + yAxis);
+
+        char initial = 'G';
+        System.out.println("initial = " + initial);
+        boolean isTeacher = true;
+
+    }
+}
