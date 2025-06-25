ca67b12a576f01a294557077a32d3161c0edfadf
Fernando Juarez <fernandojs459@gmail.com>
2025-06-25 01:43:01 -0600

    bucle for

---
+++ 02-variables-primitivas/src/bucles/Bucle.java
@@ -0,0 +1,16 @@
+package bucles;
+
+public class Bucle {
+
+    public static void main(String[] args) {
+
+        int i=0;
+        for(i=0; i<5; i++) {
+            System.out.println("Valor: " + i);
+        }
+
+        System.out.println("Sale con i=" + i);
+
+    }
+
+}
