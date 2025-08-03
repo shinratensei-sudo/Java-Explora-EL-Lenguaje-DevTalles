2e63c88ab8c2b5d7214f0888374a49a8cf846745
Fernando M90S <fernandojs459@gmail.com>
2025-08-03 14:48:11 -0600

    Java bucles

--- 02-variables-primitivas/src/bucles/Bucle.java
+++ 02-variables-primitivas/src/bucles/Bucle.java
@@ -4,12 +4,30 @@ public class Bucle {
 
     public static void main(String[] args) {
 
-        int i=0;
-        for(i=0; i<5; i++) {
+        int i = 0;
+        for (i = 0; i < 5; i++) {
             System.out.println("Valor: " + i);
         }
 
         System.out.println("Sale con i=" + i);
+
+
+        int counter = 0;
+        int acumulador = 0;
+
+        while (counter < 5) {
+            System.out.println("Counter: " + counter);
+            acumulador = acumulador + counter;
+            counter++;
+        }
+        System.out.println("Acumulador: " + acumulador);
+
+        int counter2 = 30;
+        do {
+            System.out.println("Counter 2: " + counter2);
+            counter2++;
+        } while (counter2 < 5);
+
 
     }
 
