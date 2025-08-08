477d266c10b1cfef580448da5b7ebaf7dae6d1fe
Fernando M90S <fernandojs459@gmail.com>
2025-08-07 23:13:45 -0600

    clase scanner mejores practicas

--- 02-variables-primitivas/src/scannerExample/NumberCompare.java
+++ 02-variables-primitivas/src/scannerExample/NumberCompare.java
@@ -5,19 +5,26 @@ import java.util.Scanner;
 public class NumberCompare {
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Ingrese el primer numero");
-        int a = sc.nextInt();
+        int firstNumber = scanner.nextInt();
         System.out.println("Ingrese el segundo numero");
-        int b = sc.nextInt();
-        System.out.println("La suma de a + b: " + (a+b));
-        System.out.println("La resta de a - b: " + (a-b));
+        int secondNumber = scanner.nextInt();
 
-        if(a>b) {
+        int sum = firstNumber + secondNumber;
+        int difference = firstNumber - secondNumber;
+        System.out.println("La suma de a + b: " + sum);
+        System.out.println("La resta de a - b: " + difference);
+
+        if(firstNumber >secondNumber) {
             System.out.println("el primer numero es mayor");
         }else {
             System.out.println("el segundo numero es mayor");
         }
+
+        System.out.println("fin de la app");
+
+        scanner.close();
 
 
     }
